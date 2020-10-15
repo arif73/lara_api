0515c20d33e8c9187f6f968566eddb34ea296156
somoyplatformltd <sabbir.somoy@gmail.com>
2020-10-15 19:55:59 +0600

    user registration complete

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request ){
+
+        $validation= $request->validate([
+             'name' => 'required',
+             'email' => 'required|email|unique:users',
+             'password' => 'required|min:6',
+         ]);
+ 
+        $user=User::create($validation);
+        $access_token =  $user->createToken('MyApp')->accessToken; 
+       
+        return response(['user'=>$user, 'acces_token'=>$access_token], 200);
+         
+ 
+    }
+}
--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -4,8 +4,24 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+
+    public function register(Request $request ){
+
+       $validation= $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+       $user=User::create($validation);
+       $access_token =  $user->createToken('MyApp')->accessToken; 
+      
+       return response(['user'=>$user, 'acces_token'=>$access_token], 200);
+        
+
+    }
 }
