ff284e09d399209a423d878c1a4b0b50c98fdbd7
somoyplatformltd <sabbir.somoy@gmail.com>
2020-10-16 01:56:45 +0600

    created login api

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -10,17 +10,32 @@ class AuthController extends Controller
 {
     public function register(Request $request ){
 
-        $validation= $request->validate([
+        $validatedData= $request->validate([
              'name' => 'required',
              'email' => 'required|email|unique:users',
-             'password' => 'required|min:6',
+             'password' => 'required|min:6|confirmed',
          ]);
- 
-        $user=User::create($validation);
+        $validatedData['password']=bcrypt($request->password);
+        $user=User::create($validatedData);
         $access_token =  $user->createToken('MyApp')->accessToken; 
        
-        return response(['user'=>$user, 'acces_token'=>$access_token], 200);
+        return response(['user' => $user, 'acces_token' => $access_token], 200);
          
- 
+    }
+
+    public function login(Request $request){
+
+        $loginData=$request->validate([
+            'email' => 'email|required',
+            'password' => 'required',
+        ]);
+        
+        if(!auth()->attempt($loginData)){
+            return response(['message' => 'Invalid credentials']);
+        }
+
+        $accessToken=auth()->user()->createToken('MyApp')->accessToken;
+
+        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
     }
 }
